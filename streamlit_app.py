5d45251f33767b7266e69a14ad9e7c88a9d91fc0
sri hari vasan <156992988+sri007vasan@users.noreply.github.com>
2026-01-14 12:04:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,13 @@ ingredients_list = st.multiselect(
 
 # Only run if user selected something
 if ingredients_list:
-
-    # Convert list → string
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+    smoothieroot_response = requests.get("https://my.smoothieroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothieroot_response.json(), use_container_width=True)
     # Create submit button
     time_to_insert = st.button("Submit Order")
 
@@ -46,6 +46,7 @@ if ingredients_list:
 
         st.success("Your Smoothie is ordered!", icon="✅")
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothieroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothieroot_response.json(), use_container_width=True)
+
 
