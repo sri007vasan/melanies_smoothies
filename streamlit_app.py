cf5abe7460fd75317a6be63637ae39fd915abfd1
sri hari vasan <156992988+sri007vasan@users.noreply.github.com>
2026-01-14 11:54:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,47 @@
-from snowflake.snowpark.functions import col, when_matched
+# Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
 
-# Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
-st.write("Orders that need to be filled.")
+# Page title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
 
-# Create Snowflake session
+# Name input
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+# Get Snowflake session
 cnx=st.connection("snowflake")
-session = cnx.session()
+session=cnx.session()
+# Load fruit table
+my_dataframe = session.table("smoothies.public.fruit_options")
 
-# Get only unfilled orders
-my_dataframe = (
-    session.table("smoothies.public.orders")
-           .filter(col("ORDER_FILLED") == False)
+# Convert fruit names to Python list
+fruit_list = my_dataframe.select(col("FRUIT_NAME")).to_pandas()["FRUIT_NAME"].tolist()
+
+# Multiselect
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:"
+    ,my_dataframe
+    ,max_selections=5
 )
 
-# If there are pending orders
-if my_dataframe.count() > 0:
+# Only run if user selected something
+if ingredients_list:
 
-    # Show editable table
-    editable_df = st.data_editor(my_dataframe, use_container_width=True)
+    # Convert list → string
+    ingredients_string = ''
 
-    submitted = st.button("Submit")
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
-    if submitted:
-        try:
-            # Convert edited data back to Snowpark DataFrame
-            edited_dataset = session.create_dataframe(editable_df)
+    # Create submit button
+    time_to_insert = st.button("Submit Order")
 
-            # Original Snowflake table
-            og_dataset = session.table("smoothies.public.orders")
+    if time_to_insert:
+        session.sql(
+            "INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES (?, ?)",
+            params=[ingredients_string, name_on_order]
+        ).collect()
 
-            # Merge updates
-            og_dataset.merge(
-                edited_dataset,
-                og_dataset["ORDER_UID"] == edited_dataset["ORDER_UID"],
-                [when_matched().update({"ORDER_FILLED": edited_dataset["ORDER_FILLED"]})]
-            )
-
-            st.success("Order(s) Updated!", icon="🍓")
-
-        except:
-            st.write("Something went wrong.")
-
-else:
-    st.success("There are no pending orders right now.", icon="👍")
+        st.success("Your Smoothie is ordered!", icon="✅")
